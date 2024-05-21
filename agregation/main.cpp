5398e0cb202bf149382636198de2bf12a28cbe56
hendr1an <164998945+hendr1an@users.noreply.github.com>
2024-05-21 16:12:18 +0700

    commit15

--- agregation/main.cpp
+++ agregation/main.cpp
@@ -3,3 +3,17 @@
 using namespace std;
 
 #include "anak.h"
+#include "ibu.h"
+
+int main() {
+    ibu* varIbu = new ibu("dini");
+    ibu* varIbu2 = new ibu("novi");
+    anak* varanak1 = new anak("tono");
+    anak* varanak2 = new anak("rini");
+    anak* varanak3 = new anak("Dewi");
+
+    varIbu->tambahAnak(varanak1);
+    varIbu->tambahAnak(varanak2);
+    varIbu2->tambahAnak(varanak3);
+    varIbu2->tambahAnak(varanak1);
+}
